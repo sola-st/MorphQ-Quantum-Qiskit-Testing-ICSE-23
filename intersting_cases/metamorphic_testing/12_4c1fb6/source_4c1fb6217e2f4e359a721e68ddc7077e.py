1ed7e8e6374702482fae5d03f2f048a15bb3ed2c
mattepalte <mattepalte@live.it>
2022-03-12 16:12:08 +0100

    +1 divergent bug (not reproduc on other platforms)

---
+++ intersting_cases/metamorphic_testing/12_4c1fb6/source_4c1fb6217e2f4e359a721e68ddc7077e.py
@@ -0,0 +1,34 @@
+
+# SECTION
+# NAME: PROLOGUE
+
+import qiskit
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+from qiskit.circuit.library.standard_gates import *
+from qiskit.circuit import Parameter
+
+# SECTION
+# NAME: CIRCUIT
+
+qr = QuantumRegister(5, name='qr')
+cr = ClassicalRegister(5, name='cr')
+qc = QuantumCircuit(qr, cr, name='qc')
+
+# SECTION
+# NAME: MEASUREMENT
+
+qc.measure(qr, cr)
+
+# SECTION
+# NAME: OPTIMIZATION_LEVEL
+
+from qiskit import transpile
+qc = transpile(qc, basis_gates=None, optimization_level=0, coupling_map=None)
+
+# SECTION
+# NAME: EXECUTION
+
+from qiskit import Aer, transpile, execute
+backend_04ca3b30d0b44cff8ee7c7aabe5dd837 = Aer.get_backend('qasm_simulator')
+counts = execute(qc, backend=backend_04ca3b30d0b44cff8ee7c7aabe5dd837, shots=979).result().get_counts(qc)
+RESULT = counts
